fc1880212b48b842cde9520153dc4696f9ba506e
soumya1994SN <91908101+soumya1994SN@users.noreply.github.com>
2022-09-09 23:52:17 +0530

    Update 1DBoxsolver.py

--- 1DBoxFolder/1DBoxsolver.py
+++ 1DBoxFolder/1DBoxsolver.py
@@ -25,31 +25,33 @@ t = np.linspace(tmin, tmax, Nt)
 delta_t = t[1]-t[0]
 Interval = xmax-xmin
 #Line No. 28-44 : Defining the right hand side for the discretized version of Eq.(3.2.1) (after shifting the advective term to right hand side) in terms of a tensor array and as a function of time as well as the components of the polarization vector. Stensor, Advectiontensor, Hamiltoniantensor respectively indicate tensors or multidimensional arrays which define the polarization vectors, spatial gradient term and the self-interaction Hamiltonian respectively for each spatial location and velocity mode. 
-def func(t, Polarization_vector):   
+def RHS(t, Polarization_vector):   
     S = np.reshape(Polarization_vector, N).astype(np.complex64)  
-    Stensor = np.reshape(S, (N_sp, N_vel, 3)).astype(np.complex64) 
+    Stensor = np.reshape(S, (Nsp, Nvel, 3)).astype(np.complex64) 
     Advection = np.zeros(N).astype(np.complex64)  
-    Advectiontensor = np.reshape(Advection, (N_sp, N_vel, 3)).astype(np.complex64)
-    veltensor = np.reshape(np.zeros(N_vel*2).astype(np.complex64), (N_vel, 2 ))
-    for i in range(N_vel):        
-        veltensor[i, :] = v_x[i]
+    Advectiontensor = np.reshape(Advection, (Nsp, Nvel, 3)).astype(np.complex64)
+    veltensor = np.reshape(np.zeros(Nvel*2).astype(np.complex64), (Nvel, 2 ))
+    for i in range(Nvel):        
+        veltensor[i, :] = v[i]
     veltensor[:, 0] = 1j
     Hamiltonian = np.einsum('ij,ki...->kj...', veltensor, Stensor)
-    Hamiltoniantensor = -np.einsum('ij,kj...->ki...', veltensor, Stensor)*(v_x[1]-v_x[0])
+    Hamiltoniantensor = -np.einsum('ij,kj...->ki...', veltensor, Stensor)*(v[1]-v[0])
     Crossproduct = np.cross(Hamiltoniantensor, Stensor)
-    for m in range(N_vel):
+    for m in range(Nvel):
         for n in range(3):
             Advectiontensor[:,m,n] = veltensor[m,1]*(-1.0)*sp.diff(Stensor[:,m,n], period = Interval)
         RHS = np.reshape(Advectiontensor+Crossproduct, N).astype(np.complex64)
     return RHS
+#Line No. 46: Initializing the polarization vector components using data from Initial.py code 
 InitialPolarization_vector = np.load('Initialval.npy', mmap = 'r')
-solver = spi.ode(func)
+#Line No. 48-55: Solving the set of O.D.E defined through the function "RHS" as a function of time
+solver = spi.ode(RHS)
 solver.set_integrator(name = 'zvode', method = 'BDF', nsteps = 5000000000, atol = 1e-9, rtol = 1e-9)
 arr1 = []
-solver.set_initial_value(InitialPolarization_vector, t_min)
-for i in range(N_t):
+solver.set_initial_value(InitialPolarization_vector, tmin)
+for i in range(Nt):
     solver.integrate(solver.t+delta_t)
-    arr1.append(np.reshape(solver.y, (N_sp, N_vel, 3))[:, :, :])
+    arr1.append(np.reshape(solver.y, (Nsp, Nvel, 3))[:, :, :])
     np.save("LinearApt8.npy", np.array(arr1))
 
  
