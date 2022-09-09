0fd84fc93fc41efca222e7b94572da40b2cc9f0f
soumya1994SN <91908101+soumya1994SN@users.noreply.github.com>
2022-09-09 23:14:24 +0530

    Update 1DBoxsolver.py

--- 1DBoxFolder/1DBoxsolver.py
+++ 1DBoxFolder/1DBoxsolver.py
@@ -1,3 +1,4 @@
+#Line No. 1-9 : Importing various Python libraries and solvers 
 import numpy as np
 from scipy.integrate import ode
 from scipy.integrate import complex_ode
@@ -6,24 +7,26 @@ from scipy import fftpack as sp
 from numpy import ma
 import time
 import math
+#Line No. 11-25 : Defining the configuration of the system using input parameters from Configurations1Dbox.txt 
 Configurations = np.genfromtxt('Configurations1Dbox.txt')[1, :]
-x_min = Configurations[0]
-x_max = Configurations[1]
-N_sp = Configurations[2]
-v_min = Configurations[3]
-v_max = Configurations[4]
-N_vel = Configurations[5]
-t_min = Configurations[6]
-t_max = Configurations[7]
-N_t = Configurations[8]
-N = N_sp*N_vel*3
-x_arr = np.linspace(x_min, x_max, N_sp)
-v_x = np.linspace(v_min, v_max, N_vel)
-t = np.linspace(t_min, t_max, N_t)
+xmin = Configurations[0]
+xmax = Configurations[1]
+Nsp = Configurations[2]
+vmin = Configurations[3]
+vmax = Configurations[4]
+Nvel = Configurations[5]
+tmin = Configurations[6]
+tmax = Configurations[7]
+Nt = Configurations[8]
+N = Nsp*Nvel*3
+x_arr = np.linspace(xmin, xmax, Nsp)
+v = np.linspace(vmin, vmax, Nvel)
+t = np.linspace(tmin, tmax, Nt)
 delta_t = t[1]-t[0]
+Interval = xmax-xmin
+#Line No. 28-44 : Defining the right hand side for the discretized version of Eq.(3.2.1) (after shifting the advective term to right hand side) in terms of time and the components of the polarization vector  
 def func(t, Polarization_vector):   
-    Interval = x_max-x_min
-    S = np.reshape(Polarization_vector, N).astype(np.complex64)
+    S = np.reshape(Polarization_vector, N).astype(np.complex64) #
     Stensor = np.reshape(S, (N_sp, N_vel, 3)).astype(np.complex64)
     Advection = np.zeros(N).astype(np.complex64)
     Advectiontensor = np.reshape(Advection, (N_sp, N_vel, 3)).astype(np.complex64)
